5f7c1c06e6a0e178850e65016f9bcf79989c7e63
성기범 <133200703+sungkibum@users.noreply.github.com>
2026-03-10 22:08:55 +0900

    feat: CSS_SELECTOR로 요소 찾기

--- PythonTesting/locators.py
+++ PythonTesting/locators.py
@@ -13,5 +13,10 @@ driver.find_element(By.NAME, "email").send_keys("tjdrlqja5@naver.com")
 driver.find_element(By.ID, "exampleInputPassword1").send_keys("123456")
 driver.find_element(By.ID, "exampleCheck1").click()
 
+# Xpath - //tagname[@attribute='value']   ->  //input[@type='submit']
+# CSS - tagname[attribute='value']   ->  input[type='submit'],   #id, .classname
+driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Rahul")
+driver.find_element(By.CSS_SELECTOR, "#inlineRadio1").click()
+
 
 time.sleep(5)
