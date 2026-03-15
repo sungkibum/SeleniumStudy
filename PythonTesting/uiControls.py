daa7627807ee0d155a98b6871c355bfb6ccd4e7b
성기범 <133200703+sungkibum@users.noreply.github.com>
2026-03-15 20:57:04 +0900

    feat: 라디오 버튼 자동화 방식 예제를 통해 배우기

--- PythonTesting/uiControls.py
+++ PythonTesting/uiControls.py
@@ -16,3 +16,21 @@ for checkbox in checkboxes:
         checkbox.click()
         checkbox.is_selected()
         break
+
+radioButtons = driver.find_elements(By.XPATH, "//input[@type='radio']")
+
+print(len(radioButtons))
+
+for radioButton in radioButtons:
+    if radioButton.get_attribute("value") == 'radio2':
+        radioButton.click()
+        radioButton.is_selected()
+
+        break
+
+driver.find_element(By.CSS_SELECTOR, "#hide-textbox").click()
+
+driver.find_element(By.CSS_SELECTOR, "#displayed-text").is_displayed()
+
+
+time.sleep(5)
