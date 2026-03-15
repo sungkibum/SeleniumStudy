3e16fa67ec29b5799483b162b20d7abfe88b1b77
성기범 <133200703+sungkibum@users.noreply.github.com>
2026-03-15 20:55:12 +0900

    feat: 체크박스 동적으로 처리하기

---
+++ PythonTesting/uiControls.py
@@ -0,0 +1,18 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+
+checkboxes = driver.find_elements(By.XPATH, "//input[@type='checkbox']")
+
+print(len(checkboxes))
+
+for checkbox in checkboxes:
+    if checkbox.get_attribute("value") == 'option2':
+        checkbox.click()
+        checkbox.is_selected()
+        break
