39e9564fc303d7e3f36ead5127eb350ead935d5b
성기범 <133200703+sungkibum@users.noreply.github.com>
2026-03-22 20:27:09 +0900

    feat: 프레임 다루기

---
+++ PythonTesting/framesDemo.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+driver.implicitly_wait(2)
+
+driver.get("https://the-internet.herokuapp.com/iframe")
+driver.switch_to.frame("mce_0_ifr")
+driver.find_element(By.ID, "tinymce").clear()
+driver.find_element(By.ID, "tinymce").send_keys("I am able to automate frames")
+
+driver.switch_to.default_content()
+print(driver.find_element(By.CSS_SELECTOR, "h3").text)
+
+driver.close()
