e530ed805c12cf309dbd852fa9e8c1d7ab468f55
성기범 <133200703+sungkibum@users.noreply.github.com>
2026-07-28 22:01:15 +0900

    feat: [Pytest] 코드 작성 완료

---
+++ PythonTesting/pytestsDemo/test_demo1.py
@@ -0,0 +1,6 @@
+# Any pytest file sould start with test_ or end with _test
+# pytest method names should start with test
+# Any code should be wrapped in method only
+
+def test_firstProgram():
+  print("Hello")
