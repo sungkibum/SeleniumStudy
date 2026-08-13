482d00907ec339d30b6f4af4e4d56f777dbf998c
성기범 <133200703+sungkibum@users.noreply.github.com>
2026-08-13 22:07:20 +0900

    feat: smoke test and xfail

--- PythonTesting/pytestsDemo/test_demo1.py
+++ PythonTesting/pytestsDemo/test_demo1.py
@@ -2,8 +2,13 @@
 # pytest method names should start with test
 # Any code should be wrapped in method only
 
+import pytest
+
+
+@pytest.mark.smoke
 def test_firstProgram():
   print("Hello")
 
+@pytest.mark.xfail
 def test_SecondGreetCreditCard():
   print("Good Morning")
--- PythonTesting/pytestsDemo/test_demo2.py
+++ PythonTesting/pytestsDemo/test_demo2.py
@@ -4,9 +4,16 @@
 # Method name should have sense
 # -k stands for method names execution, -s logs in out put  -v stands for more info metadata
 # you can run specific file with py.test <filename>
+# you can mark (tag) tests @pytest.mark.smoke and then run with -m
+# you can skip tests with @pytest.mark.skip
+# @pytest.mark.xfail
+import pytest
 
+
+@pytest.mark.smoke
+@pytest.mark.skip
 def test_firstProgram():
-  msg = "Hello"
+  msg = "Hello" #operations
   assert msg == "Hi", "Test failed   because strings do not match"
 
 
