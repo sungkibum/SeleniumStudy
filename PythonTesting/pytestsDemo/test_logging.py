e15af323fdae0aa83823aae2c6b1e8cffa22b418
성기범 <133200703+sungkibum@users.noreply.github.com>
2026-08-23 15:06:35 +0900

    feat: logfile setting

--- PythonTesting/pytestsDemo/test_logging.py
+++ PythonTesting/pytestsDemo/test_logging.py
@@ -1,14 +1,18 @@
 import logging
 
-logger = logging.getLogger(__name__)
+def test_logging():
+  logger = logging.getLogger(__name__)
 
-fileHandler = logging.FileHandler("logfile.log")
+  fileHandler = logging.FileHandler("logfile.log")
+  formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
+  fileHandler.setFormatter(formatter)
 
-logger.addHandler(fileHandler)  #filahandler object
+  logger.addHandler(fileHandler)  #filahandler object
 
 
-logger.debug("A debug statement is executed")
-logger.info("Information statement")
-logger.warning("Something is in warning mode")
-logger.error("A Major error has happened")
-logger.critical("Critical issue")
+  logger.setLevel(logging.INFO)
+  logger.debug("A debug statement is executed")
+  logger.info("Information statement")
+  logger.warning("Something is in warning mode")
+  logger.error("A Major error has happened")
+  logger.critical("Critical issue")
