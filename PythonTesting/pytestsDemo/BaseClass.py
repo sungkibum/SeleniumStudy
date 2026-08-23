5606db6edd7f958ec3063bc8237fd5349dd54e90
성기범 <133200703+sungkibum@users.noreply.github.com>
2026-08-23 22:52:49 +0900

    feat: test코드에 로깅 추가

---
+++ PythonTesting/pytestsDemo/BaseClass.py
@@ -0,0 +1,16 @@
+import logging
+
+
+class BaseClass:
+  def getLogger(self):
+    logger = logging.getLogger(__name__)
+
+    fileHandler = logging.FileHandler("logfile.log")
+    formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
+    fileHandler.setFormatter(formatter)
+
+    logger.addHandler(fileHandler)  #filahandler object
+
+
+    logger.setLevel(logging.INFO)
+    return logger
--- PythonTesting/pytestsDemo/test_fixturesData.py
+++ PythonTesting/pytestsDemo/test_fixturesData.py
@@ -1,11 +1,16 @@
 
 import pytest
 
+from BaseClass import BaseClass
+
+
+
 
 @pytest.mark.usefixtures("dataLoad")
-class TestExample2:
+class TestExample2(BaseClass):
 
   def test_editProfile(self, dataLoad):
-    print(dataLoad[0])
-    print(dataLoad[1])
+    log = self.getLogger()
+    log.info(dataLoad[0])
+    log.info(dataLoad[2])
     print(dataLoad[2])
