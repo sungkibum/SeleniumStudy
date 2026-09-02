c1860d64e78f6dc3244e78926b506dc9e21b2214
성기범 <133200703+sungkibum@users.noreply.github.com>
2026-09-02 23:36:21 +0900

    fix: import 수정

--- PythonSelFramework/tests/test_e2e.py
+++ PythonSelFramework/tests/test_e2e.py
@@ -2,7 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-
+from selenium.webdriver.common.by import By
 from PythonSelFramework.utilities.BaseClass import BaseClass
 
 
