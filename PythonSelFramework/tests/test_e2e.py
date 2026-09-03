d2abc4a60696277e7ffc990391b29514bd5c242e
성기범 <133200703+sungkibum@users.noreply.github.com>
2026-09-03 21:51:43 +0900

    fix: 구형버전 코드 신형으로 업데이트

--- PythonSelFramework/tests/test_e2e.py
+++ PythonSelFramework/tests/test_e2e.py
@@ -7,31 +7,32 @@ from selenium.webdriver.common.by import By
 from utilities.BaseClass import BaseClass
 
 
-#@pytest.mark.usefixtures("setup")
 class TestOne(BaseClass):
 
-  def test_e2e(self):
-    self.find_element_by_css_selector("a[href*='shop']").click()
-    cards = self.driver.find_elements_by_css_selector(".card-title a")
-    i = -1
-    for card in cards:
-      i = i + 1
-      cardText = card.text
-      print(cardText)
-      if cardText == "Blackberry":
-        self.driver.find_elements_by_css_selector(".card-footer button")[i].click()
+    def test_e2e(self):
+        self.driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click()
 
+        cards = self.driver.find_elements(By.CSS_SELECTOR, ".card-title a")
+        i = -1
+        for card in cards:
+            i = i + 1
+            cardText = card.text
+            print(cardText)
+            if cardText == "Blackberry":
+                self.driver.find_elements(By.CSS_SELECTOR, ".card-footer button")[i].click()
 
-    self.driver.find_element_by_css_selector("a[class*='btn-primary']").click()
+        self.driver.find_element(By.CSS_SELECTOR, "a[class*='btn-primary']").click()
 
-    self.driver.find_element_by_xpath("//button[@class='btn btn-success']").click()
-    self.driver.find_element_by_id("country").send_keys("ind")
-    # time.sleep(5)
-    element = WebDriverWait(self.driver, 10).until(
-      EC.presence_of_element_located((By.LINK_TEXT, "India")))
-    self.driver.find_elemet_by_link_text("India").click()
-    self.driver.find_element_by_xpath("//div[@class='checkbox checkbox-primary']").click()
-    self.driver.find_element_by_css_selector("[type='submit']").click()
-    textMatch = self.driver.find_element_by_css_selector("[class*='alert-success']").text
+        self.driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
+        self.driver.find_element(By.ID, "country").send_keys("ind")
 
-    assert ("Success! Thank you!" in textMatch)
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.LINK_TEXT, "India")))
+        self.driver.find_element(By.LINK_TEXT, "India").click()
+
+        self.driver.find_element(By.XPATH, "//div[@class='checkbox checkbox-primary']").click()
+        self.driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+
+        textMatch = self.driver.find_element(By.CSS_SELECTOR, "[class*='alert-success']").text
+
+        assert ("Success! Thank you!" in textMatch)
