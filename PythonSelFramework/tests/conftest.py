495df918d80b21a9c3d045bed7f8a9b61f196d53
성기범 <133200703+sungkibum@users.noreply.github.com>
2026-09-04 23:55:50 +0900

    feat: 브라우저 선택을 위한 명령행 인자 전달

--- PythonSelFramework/tests/conftest.py
+++ PythonSelFramework/tests/conftest.py
@@ -2,10 +2,22 @@ from selenium import webdriver
 
 import pytest
 
+def pytest_addoption(parser):
+  parser.addoption(
+    "--browser_name", action="store", default="chrome"
+  )
+
 
 @pytest.fixture(scope="class")
 def setup(request):
-  driver = webdriver.Chrome()
+  browser_name = request.config.getoption("browser_name")
+  if browser_name == "chrome":
+    driver = webdriver.Chrome()
+  elif browser_name == "firefox":
+    driver = webdriver.Firefox()
+  elif browser_name == "edge":
+    driver = webdriver.Edge()
+
   driver.get("https://rahulshettyacademy.com/angularpractice/")
   driver.maximize_window()
   request.cls.driver = driver
