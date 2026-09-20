c27cd19bd15ba5e522277c5ac522ec56668ebbf8
성기범 <133200703+sungkibum@users.noreply.github.com>
2026-09-20 23:08:39 +0900

    refactor: 하드코딩값 제거, 데이터 기반 디자인 구현

--- PythonSelFramework/tests/test_HomePage.py
+++ PythonSelFramework/tests/test_HomePage.py
@@ -1,16 +1,23 @@
+import pytest
+
 from utilities.BaseClass import BaseClass
 from pageObjects.HomePage import HomePage
 
 
 class TestHomePage(BaseClass):
 
-    def test_formSubmission(self):
+    def test_formSubmission(self, getData):
         homePage = HomePage(self.driver)
-        homePage.getName().send_keys("Rahul")
-        homePage.getEmail().send_keys("shetty")
+        homePage.getName().send_keys(getData[0])
+        homePage.getEmail().send_keys(getData[1])
         homePage.getExampleCheck1().click()
-        self.selectOptionByText(homePage.getGender(), "Male")
+        self.selectOptionByText(homePage.getGender(), getData[2])
         homePage.clickSubmit()
 
         alertText = homePage.getSuccessAlertText()
         assert ("Success" in alertText)
+        self.driver.refresh()
+
+    @pytest.fixture(params=[("Rahul", "shetty", "Male"), ("Anshika", "shetty", "Female")])
+    def getData(self, request):
+        return request.param
